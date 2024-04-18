5ab69997926b7af2e01c3ebeb3400c38892c185d
akshatjain04 <akshatjain0410@gmail.com>
2024-04-18 11:53:38 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-4-1106-preview

--- source/src/main/java/architecture/group/GroupService.java
+++ source/src/main/java/architecture/group/GroupService.java
@@ -11,24 +11,29 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class GroupService {
-    public Map<Object, Map<String, Object>> groupPropertiesBy(Object object, String contains) {
-        final var result = new HashMap<Object, Map<String, Object>>();
 
-        Arrays.stream(object.getClass().getDeclaredFields()).filter(field -> field.getName().contains(contains)).forEach(field -> {
-            final var wrapper = new BeanWrapperImpl(object);
-            final var name = field.getName().replace(contains, "");
-            result.computeIfAbsent(wrapper.getPropertyValue(field.getName()), key -> new HashMap<>()).put(name, wrapper.getPropertyValue(name));
-        });
+	public Map<Object, Map<String, Object>> groupPropertiesBy(Object object, String contains) {
+		final var result = new HashMap<Object, Map<String, Object>>();
 
-        return result;
-    }
+		Arrays.stream(object.getClass().getDeclaredFields())
+			.filter(field -> field.getName().contains(contains))
+			.forEach(field -> {
+				final var wrapper = new BeanWrapperImpl(object);
+				final var name = field.getName().replace(contains, "");
+				result.computeIfAbsent(wrapper.getPropertyValue(field.getName()), key -> new HashMap<>())
+					.put(name, wrapper.getPropertyValue(name));
+			});
 
-    public void setProperties(Object object, Map<String, Object> map) {
-        final var wrapper = new BeanWrapperImpl(object);
-        map.forEach((property, value) -> {
-            if (Objects.nonNull(value)) {
-                wrapper.setPropertyValue(property, value);
-            }
-        });
-    }
+		return result;
+	}
+
+	public void setProperties(Object object, Map<String, Object> map) {
+		final var wrapper = new BeanWrapperImpl(object);
+		map.forEach((property, value) -> {
+			if (Objects.nonNull(value)) {
+				wrapper.setPropertyValue(property, value);
+			}
+		});
+	}
+
 }
