76573a939e5aebea1b1443d243a11b18ec54e09a
akshatjain04 <akshatjain0410@gmail.com>
2024-04-21 02:59:47 +0530

    API test (Using API Spec) generated by RoostGPT
    Using AI Model gpt-4-1106-preview

--- source/src/main/java/architecture/group/Person.java
+++ source/src/main/java/architecture/group/Person.java
@@ -5,8 +5,13 @@ import lombok.Data;
 
 @Data
 public class Person {
-    private String passport;
-    private LocalDate passportExpirationDate;
-    private String driverLicense;
-    private LocalDate driverLicenseExpirationDate;
+
+	private String passport;
+
+	private LocalDate passportExpirationDate;
+
+	private String driverLicense;
+
+	private LocalDate driverLicenseExpirationDate;
+
 }
--- source/src/main/java/architecture/invoice/dtos/InvoiceDto.java
+++ source/src/main/java/architecture/invoice/dtos/InvoiceDto.java
@@ -7,9 +7,15 @@ import lombok.Data;
 
 @Data
 public class InvoiceDto {
-    private Long id;
-    private String number;
-    private LocalDateTime dateTime;
-    private InvoiceStatus status;
-    private List<InvoiceItemDto> items;
+
+	private Long id;
+
+	private String number;
+
+	private LocalDateTime dateTime;
+
+	private InvoiceStatus status;
+
+	private List<InvoiceItemDto> items;
+
 }
