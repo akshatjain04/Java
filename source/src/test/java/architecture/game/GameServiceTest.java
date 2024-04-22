fbc4cc4af8b715914077fa0ca571d49f280cf151
akshatjain04 <akshatjain0410@gmail.com>
2024-04-22 16:33:59 +0530

    API test (Using API Spec) generated by RoostGPT
    Using AI Model gpt-4-1106-preview

--- source/src/test/java/architecture/game/GameServiceTest.java
+++ source/src/test/java/architecture/game/GameServiceTest.java
@@ -7,13 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = { GameService.class, GameRepository.class })
 class GameServiceTest {
-    @Autowired
-    GameService gameService;
 
-    @Test
-    void testList() {
-        final var title = "Game A";
-        final var games = gameService.list(new Game(title));
-        Assertions.assertEquals(title, games.get(0).getTitle());
-    }
+	@Autowired
+	GameService gameService;
+
+	@Test
+	void testList() {
+		final var title = "Game A";
+		final var games = gameService.list(new Game(title));
+		Assertions.assertEquals(title, games.get(0).getTitle());
+	}
+
 }
